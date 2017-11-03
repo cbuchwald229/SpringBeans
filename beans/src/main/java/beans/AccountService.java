1a9454d9d3a416b5ca250c2c1fa18842e8134afb
cbuchwald229 <31973954+cbuchwald229@users.noreply.github.com>
2017-11-02 22:44:54 -0500

    Delete AccountService.java

--- beans/src/main/java/beans/AccountService.java
+++
@@ -1,9 +0,0 @@
-package beans;
-
-public interface AccountService {
-	
-	public void transferMoney(long sourceAccountId, long targetAccountId, double amount);
-	public void depositMoney(long accountId, double amount) throws Exception;
-	public Account getAccount(long accountId);
-
-}
